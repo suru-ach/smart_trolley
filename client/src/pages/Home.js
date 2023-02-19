52f5525f139e5958252975c784ac0658bb8c9552
Srivatsa R Upadhya <srivatsarupadhya@gmail.com>
2023-02-19 18:23:29 +0530

    Login features added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,38 +1,46 @@
 import { DarkThemeToggle, } from "flowbite-react";
 import { useTheme, useThemeMode, } from "flowbite-react";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import CarouselComponent from "../components/Carousel";
 import CardComponent from "../components/Card";
 import FooterComponent from "../components/Footer";
+import { Navigate } from "react-router-dom";
 
 
 export default function Home() {
-    return (
-        <>
-            <React.Fragment>
-                <Navbar></Navbar>
-                <DarkThemeToggle className="sticky top-[90vh]"></DarkThemeToggle>
-                <div className={`w-[85%] m-auto`}>
+    const Loggedcookie = JSON.parse(document.cookie).loggedIn;
+    if (Loggedcookie) {
 
-                    <CarouselComponent></CarouselComponent>
-                    <h2 className="text-4xl text-center m-20">Deals</h2>
-                    <div className="overflow-x-scroll flex">
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
-                        <CardComponent></CardComponent>
+        return (
+            <>
+                <React.Fragment>
+                    <Navbar></Navbar>
+                    <DarkThemeToggle className="sticky top-[90vh]"></DarkThemeToggle>
+                    <div className={`w-[85%] m-auto`}>
+
+                        <CarouselComponent></CarouselComponent>
+                        <h2 className="text-4xl text-center m-20">Deals</h2>
+                        <div className="overflow-x-scroll flex">
+                            <CardComponent></CardComponent>
+                            <CardComponent></CardComponent>
+                            <CardComponent></CardComponent>
+                            <CardComponent></CardComponent>
+                            <CardComponent></CardComponent>
+                            <CardComponent></CardComponent>
+                            <CardComponent></CardComponent>
+                            <CardComponent></CardComponent>
+                        </div>
                     </div>
-                </div>
 
-                <div className="mt-10">
-                    <FooterComponent></FooterComponent>
-                </div>
-            </React.Fragment>
-        </>
-    )
+                    <div className="mt-10">
+                        <FooterComponent></FooterComponent>
+                    </div>
+                </React.Fragment>
+            </>
+        )
+    }
+    else{
+        window.location.replace("http://localhost:3000/")
+    }
 }
