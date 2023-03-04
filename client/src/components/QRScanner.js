1148e74f8488b37af4522b73e10e0f7ea535e2c9
Srivatsa R Upadhya <srivatsarupadhya@gmail.com>
2023-03-04 21:30:13 +0530

    Minor changes

--- client/src/components/QRScanner.js
+++ client/src/components/QRScanner.js
@@ -4,7 +4,10 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 
 export default function QRScannerComponent() {
-  const [ShowQRReader, setShowQRReader] = useState(true);
+
+  const [ShowQRReader, setShowQRReader] = useState(localStorage.getItem('cartID'));
+  console.log(localStorage.getItem('cartID'));
+
   const setLocalStorage = (data) => {
     for (const key in data) {
       window.localStorage.setItem(key, data[key]);
@@ -24,27 +27,22 @@ export default function QRScannerComponent() {
       axios.post(`${process.env.REACT_APP_SERVER_URL}/api/cartIDSubmit`, { cartID: decodedText, phone })
         .then((data) => {
           setLocalStorage(data.data.data)
+          setShowQRReader(true)
         })
         .catch(err => {
           console.log(err);
         })
-        
-        setShowQRReader(false);
     };
-    if (!localStorage.getItem('cartID')) {
+    if (!ShowQRReader) {
       const config = { fps: 30, qrbox: { width: 150, height: 150 } };
       html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
     }
-    else{
-      setShowQRReader(false)
-    }
-
 
   })
 
   return (
     <>
-      {(ShowQRReader &&
+      {(!ShowQRReader &&
         <h2 className="text-center">Scan Cart QRCode</h2>
       )}
       <div id="reader" className="w-64 m-auto rounded mb-5"></div>
