6d6397a7a909b6e7c1afe9b5344298fe0d644a6b
sonith-shetty <sonithshetty.js@gmail.com>
2023-03-05 23:28:19 +0530

    modified navbar and storepage

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,35 +3,9 @@ import { Footer } from "flowbite-react";
 export default function FooterComponent() {
     return (
         <>
-            <Footer container={true}>
-                <div className="w-full text-center">
-                    <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-                        <Footer.Brand
-                            href="#"
-                            src="./Images/Mypr.png"
-                            alt="Smart_Trolley logo"
-                            name="Smart Trolley"
-                        />
-                        <Footer.LinkGroup>
-                            <Footer.Link href="about">
-                                About
-                            </Footer.Link>
-                            <Footer.Link href="https://policies.google.com/privacy">
-                                Privacy Policy
-                            </Footer.Link>
-                            <Footer.Link href="#">
-                                Licensing
-                            </Footer.Link>
-                        </Footer.LinkGroup>
-                    </div>
-                    <Footer.Divider />
-                    <Footer.Copyright
-                        href="#"
-                        by="Smart Trolley™"
-                        year={2023}
-                    />
-                </div>
-            </Footer>
+            <footer class="flex-all-center">
+                <p> copyright reserved &copy; smart trolley.com</p>
+            </footer>
         </>
     )
 }
