70510b0d03c27b41aacf5336debd51e1ab35d57c
Taksheelshetty <120228028+Taksheelshetty@users.noreply.github.com>
2023-03-11 18:02:19 +0530

    Merge branch 'websocket' of https://github.com/Taksheelshetty/smart_trolley into websocket

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,31 +1,40 @@
-import { DarkThemeToggle, Tooltip} from "flowbite-react";
+import { DarkThemeToggle, Tooltip } from "flowbite-react";
 import { useTheme, useThemeMode, } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import CarouselComponent from "../components/Carousel";
 import CardComponent from "../components/Card";
 import FooterComponent from "../components/Footer";
 import Carscommponent from "../components/cars";
+import Aos from "aos";
+import 'aos/dist/aos.css'
+
+
 
 
 export default function Home() {
+    useEffect(() => {
+        Aos.init();
+    }, [])
 
     return (
         <>
+
             <React.Fragment>
-                
+
                 <DarkThemeToggle className="sticky top-[90vh]"></DarkThemeToggle>
                 <div className={`p-0 my-0`}>
-                    <CarouselComponent></CarouselComponent>
-                    <div  className="text-4xl align-center text-center m-20 p-8" >
+                    <div data-aos="fade-up">
+                        <CarouselComponent></CarouselComponent>
+                    </div>
+                    <div className="text-4xl align-center text-center m-10 p-8" data-aos="fade-up" data-aos-delay="100">
                         <Tooltip
                             content="Select any categories"
                             placement="top"
                         >
-                            <h1>Shop top categories</h1>
+                            <h1 className="text center focus:outline-none focus:ring focus:ring-violet-300 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:">Shop top categories</h1>
                         </Tooltip>
                     </div>
-
                     <div data-aos="fade-up" data-aos-delay="100">
                         <Carscommponent></Carscommponent>
                         <h2 className=" my-4 text-4xl text-center focus:outline-none focus:ring focus:ring-violet-300 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:">Deals</h2>
@@ -33,8 +42,6 @@ export default function Home() {
 
 
                     <div className="overflow-x-scroll flex space-x-4 " data-aos="flip-left" data-aos-delay="10" >
-
-                   
                         <CardComponent></CardComponent>
                         <CardComponent></CardComponent>
                         <CardComponent></CardComponent>
@@ -47,5 +54,5 @@ export default function Home() {
         </>
     )
 }
-   
+
 
