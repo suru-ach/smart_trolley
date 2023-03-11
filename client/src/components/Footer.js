196a309c36101d79033eac1057b2ca07fbefe7da
Taksheelshetty <120228028+Taksheelshetty@users.noreply.github.com>
2023-03-11 18:08:05 +0530

    Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Footer } from "flowbite-react";
 export default function FooterComponent() {
     return (
         <>
-            <footer class="flex-all-center">
+            <footer class=" text-center p-8">
                 <p> copyright reserved &copy; smart trolley.com</p>
             </footer>
         </>
